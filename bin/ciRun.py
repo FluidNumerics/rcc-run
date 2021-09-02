8fa8261eda7dc6afaad0b969b12067c7da0f2e71
Joe Schoonover <joe@fluidnumerics.com>
2021-09-02 03:58:47 +0000

    Remove call to spindownComputeNodes
    * FluidNumerics/slurm-gcp has been updated to include a shutdown-script
    that deletes any compute nodes from the cluster that are still up and
    running.

--- bin/ciRun.py
+++ bin/ciRun.py
@@ -141,52 +141,6 @@ def run(cmd):
 
 #END run
 
-def spindownComputeNodes():
-    """Looks for compute nodes associated with a Slurm cluster and deletes the instances"""
-    import googleapiclient.discovery
-
-    scontrol = '/usr/local/bin/scontrol'
-    config = json.loads(get_instance_metadata('config'))
-    compute = googleapiclient.discovery.build('compute', 'v1',
-                                              cache_discovery=False)
-
-    page_token = ""
-    g_nodes = []
-    for partition in config['partitions']:
-        while True:
-            resp = compute.instances().list(project=config['project'],
-                      zone=partition['zone'], pageToken=page_token,
-                      filter='name={}-compute*'.format(config['cluster_name'])).execute()
-
-            if "items" in resp:
-                g_nodes.extend(resp['items'])
-            if "nextPageToken" in resp:
-                page_token = resp['nextPageToken']
-                continue
-
-            break;
-
-    if(len(g_nodes)):
-        for node in g_nodes:
-            cmd = "{} update node={} state=power_down".format(scontrol, node['name'])
-            stdout, stderr, rc = run(cmd)
-            if rc == 0:
-                try:
-                    print(stdout.decode('utf-8'))
-                except:
-                    print(stdout)
-            else:
-                try:
-                    print(stdout.decode('utf-8'))
-                    print(stderr.decode('utf-8'))
-                except:
-                    print(stdout)
-                    print(stderr)
-              
-
-
-#END spindownComputeNodes
-
 def slurmgcpRun(settings,tests):
     """Executes command_groups sequentially on a Slurm-GPC cluster"""
 
@@ -380,9 +334,6 @@ def slurmgcpRun(settings,tests):
     with open(WORKSPACE+'/results.json','w')as f:          
         f.write(json.dumps(tests))
 
-
-    # Spindown any remaining compute nodes
-    spindownComputeNodes()
 
 #END slurmgcpRun
 
