ff0487b73a8f805d8599040e3ddf366d2d2a074c
Joe Schoonover <joe@fluidnumerics.com>
2021-09-03 01:17:17 +0000

    Add step to stop controller before deleting

--- bin/fluid-cicb.py
+++ bin/fluid-cicb.py
@@ -343,6 +343,13 @@ def deprovisionCluster():
  
     clusterType = settings['cluster_type']
     print('Deprovisioning cluster...',flush=True)
+
+    # Stop controller so that shutdown script runs
+    # The shutdown script deletes compute nodes
+    if settings['cluster_type'] == 'rcc-ephemeral':
+        cmd = 'gcloud compute instance stop {} --zone={}'.format(settings['hostname'],settings['zone'])
+        localRun(cmd)
+
     os.chdir(TFPATH+clusterType)
     localRun('terraform destroy --auto-approve')
     print('Done deprovisioning cluster',flush=True)
